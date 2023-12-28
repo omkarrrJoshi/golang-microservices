9c9906862e9e07d2deb461d381aa132890c77a76
omkarrrJoshi <one.omkarjoshi@gmail.com>
2023-12-28 23:20:33 +0530

    mock functions and use interface for same

--- mvc/model/user_dao.go
+++ mvc/model/user_dao.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/omkarrrJoshi/golang-microservices/mvc/utils"
@@ -11,9 +12,21 @@ var (
 	users = map[int64]*User{
 		123: {Id: 123, FirstName: "omkar", LastName: "joshi", Email: "o.j@gmail.com"},
 	}
+	UserDao userDaoInterface
 )
 
-func GetUser(userId int64) (*User, *utils.ApplicationError) {
+func init() {
+	UserDao = &userDao{}
+}
+
+type userDaoInterface interface {
+	GetUser(int64) (*User, *utils.ApplicationError)
+}
+
+type userDao struct{}
+
+func (u *userDao) GetUser(userId int64) (*User, *utils.ApplicationError) {
+	log.Println("we are accessing the databse")
 	user, ok := users[userId]
 	if ok {
 		return user, nil
